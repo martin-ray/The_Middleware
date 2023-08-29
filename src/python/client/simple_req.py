c3df0aee11d0b602c658b316727d12dbc05df62f
martin-ray <aorayagi@gmail.com>
2023-08-29 15:30:16 +0900

    add matplotlib

--- src/python/client/simple_req.py
+++ src/python/client/simple_req.py
@@ -1,6 +1,7 @@
 import requests
 import _mgard as mgard
 import numpy as np
+import matplotlib.pyplot as plt
 
 # URL of the server
 url = "http://localhost:8080"
@@ -11,21 +12,29 @@ while True:
     x = int(input("Enter x: "))
     y = int(input("Enter y: "))
     z = int(input("Enter z: "))
+    tol = float(input("Enter tol:"))
     
     # Define parameters for the request
     params = {
         'time': time,
         'x': x,
         'y': y,
-        'z': z
+        'z': z,
+        'tol':tol
     }
     
     # Send the HTTP GET request
     response = requests.get(url, params=params)
     response_content = response.content
     numpy_array = np.frombuffer(response_content, dtype=np.uint8)
-    decompressed = mgard.decompress(numpy_array)
+    decompressed = mgard.decompress(numpy_array).astype(np.float32)
     print("size decompressed data:",decompressed.nbytes)
+
+    # convert to 2d
+    data = decompressed[0,:,:]
+    plt.imshow(data, cmap='jet')
+    plt.title("tol={}".format(tol))
+    plt.savefig("./fig_tol={}.png".format(tol))
 
     # Process the response if needed
     if response.status_code == 200:
