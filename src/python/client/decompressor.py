5290d36f102e8219af8ca48cec37f1143d2f493f
martin-ray <aorayagi@gmail.com>
2023-10-05 14:43:57 +0900

    add normal decompression which is conducted by L1Prefetcher

--- src/python/client/decompressor.py
+++ src/python/client/decompressor.py
@@ -2,7 +2,8 @@ import _mgard as mgard
 import numpy as np
 
 class Decompressor:
-    def __init__(self,device = "GPU") -> None:
+    def __init__(self,L1Cache,device = "GPU") -> None:
+        self.L1Cache = L1Cache
         # configure device
         self.config = mgard.Config()
         if device == "OMP":
@@ -11,7 +12,9 @@ class Decompressor:
             self.config.dev_type = mgard.DeviceType.SERIAL
 
     def decompress_req(self,compressed):
-        pass
+        decompressed = mgard.decompress(compressed).astype(np.float32)
+        self.L1Cache.put(decompressed)
+        return
     
 
     def decompress_req_urgent(self,compressed):
