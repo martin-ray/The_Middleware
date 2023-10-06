5622395518050668fc226efb18562a381468e8f3
martin-ray <aorayagi@gmail.com>
2023-10-06 18:29:24 +0900

    add recompose mechanism.

--- src/python/client/recomposer.py
+++ src/python/client/recomposer.py
@@ -3,29 +3,36 @@ import numpy as np
 class Recomposer:
     def __init__(self, blockOffset):
         self.blockOffset = blockOffset
+        self.xyzSize = 1024
 
     # input is sets of (blockId,blockdata) 
-    def recompose(self,blocksData):
+    def recompose(self,blocksData,xyzRange):
+        xStart = xyzRange[0]
+        xEnd = xyzRange[1]
+        yStart = xyzRange[2]
+        yEnd = xyzRange[3]
+        zStart = xyzRange[4]
+        zEnd = xyzRange[5]
+
+        # create a empty block to fill in
+        synthesized_block = np.zeros((xEnd - xStart + 1), (yEnd - yStart + 1), (zEnd - zStart + 1)) 
+        
         # Determine the dimensions of the synthesized block based on the maximum blockId
-        max_x, max_y, max_z = (0, 0, 0)
+        min_x, min_y, min_z = (self.xyzSize, self.xyzSize, self.xyzSize)
         for block_id, _ in blocksData:
             x, y, z = block_id
-            max_x = max(max_x, x)
-            max_y = max(max_y, y)
-            max_z = max(max_z, z)
-        
-        # Initialize the synthesized block with zeros
-        synthesized_block = np.zeros(((max_x + 1) * self.blockOffset, (max_y + 1) * self.blockOffset, (max_z + 1) * self.blockOffset))
-        
+            min_x = min(min_x, x)
+            min_y = min(min_y, y)
+            min_z = min(min_z, z)
+
         # Fill in the synthesized block with block data
         for block_id, block_data in blocksData:
             x, y, z = block_id
-            x_offset = x * self.blockOffset
-            y_offset = y * self.blockOffset
-            z_offset = z * self.blockOffset
+            xPoint = x - min_x
+            yPoint = y - min_y
+            zPoint = z - min_z
             block_shape = block_data.shape
-            synthesized_block[x_offset:x_offset+block_shape[0], y_offset:y_offset+block_shape[1], z_offset:z_offset+block_shape[2]] = block_data
-        
+            synthesized_block[xPoint:xPoint+block_shape[0],yPoint:yPoint+block_shape[1], zPoint:zPoint+block_shape[2]] = block_data
         return synthesized_block
 
 
