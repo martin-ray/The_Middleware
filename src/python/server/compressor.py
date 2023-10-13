6c41007ed82783b27cd5f120a8566230dbd5d929
martin-ray <aorayagi@gmail.com>
2023-10-13 12:30:31 +0900

    compressorをどうやって実装するかまよっているという話

--- src/python/server/compressor.py
+++ src/python/server/compressor.py
@@ -1,9 +1,14 @@
 import _mgard as mgard
+from collections import deque
 
+
+# コンプレッサーの扱い方について今迷っています。i/oみたいに、一回一回、お願いするか、キューみたいなのを用意しておいて、
+# 別スレッドで動いているコンプレッサーが自分でキューからデータを取ってくるか。どっちにするか。
 class compressor:
     def __init__(self,L3Cache,device = "GPU") -> None:
         self.L3Cache = L3Cache
-
+        self.originalQ = deque()
+        
         # configure device
         self.config = mgard.Config()
         if device == "OMP":
@@ -11,12 +16,13 @@ class compressor:
         elif device == "SERIAL":
             self.config.dev_type = mgard.DeviceType.SERIAL
     
-    def compress_req(self,blockId,original,tol):
-        compressed = mgard.compress(original, tol, 0, mgard.ErrorBoundType.REL, self.config)
+    def compress_req(self,blockId,original):
+        tol = blockId[0]
+        compressed = mgard.compress(original,tol, 0, mgard.ErrorBoundType.REL, self.config)
         self.L3Cache.put(blockId,compressed)
         return
     
-    def decompress(self,original):
+    def compress(self,original,tol):
         compressed = mgard.compress(original, tol, 0, mgard.ErrorBoundType.REL, self.config)
         return compressed
     
