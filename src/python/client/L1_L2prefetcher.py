3516a0e7b15d0246798b67996343f882fd026ca4
martin-ray <aorayagi@gmail.com>
2023-10-20 19:56:51 +0900

    change file names

---
+++ src/python/client/L1_L2prefetcher.py
@@ -0,0 +1,293 @@
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+import queue
+from collections import deque
+import asyncio
+import numpy as np
+import threading
+# self-defined libraries
+import _mgard as mgard
+from L1_L2Cache import LRU_cache
+# from L1_L2Cache import dynamic_cache
+from NetInterface import NetIF
+from decompressor import Decompressor
+
+class L2Prefetcher:
+    def __init__(self,L2Cache,maxTimestep=9,serverURL="http://localhost:8080") -> None:
+        self.URL = serverURL
+
+        # ToleranceArray
+        self.Tols = [0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5]
+        self.maxTimestep = maxTimestep
+        self.maxX = 1024
+        self.maxY = 1024
+        self.maxZ = 1024
+        self.default_offset = 256
+        self.L2Cache = L2Cache
+        self.gonnaPrefetchSet = set()
+        self.prefetchedSet = set()
+        self.prefetch_q = deque() # blocks going to get
+        self.Netif = NetIF(self.L2Cache,serverURL)
+        self.stop_thread = False
+
+        # フェッチループを起動
+        if self.L2Cache.capacity == 0:
+            pass
+        else:
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            self.enqueue_first_blockId()
+
+    
+    def enque_neighbor_blocks(self,centerBlock):
+        tol = centerBlock[0] 
+        timestep = centerBlock[1]
+        x = centerBlock[2]
+        y = centerBlock[3]
+        z = centerBlock[4]
+
+        ## TODO tolの扱い
+        for dt in [-1,0,1]:
+            for dx in [-self.default_offset, 0, self.default_offset]:
+                for dy in [-self.default_offset, 0, self.default_offset]:
+                    for dz in [-self.default_offset, 0, self.default_offset]:
+                        if (self.gonnaPrefetchSet.__contains__((tol,timestep+dt, x+dx, y+dy, z+dz))
+                            or (timestep+dt < 0) or (timestep+dt >= self.maxTimestep) 
+                            or (x+dx < 0) or (x+dx >= self.maxX)
+                            or (y+dy < 0) or (y+dy >= self.maxY)
+                            or (z+dz < 0) or (z+dz >= self.maxZ)):
+                            continue
+                        else:
+                            self.prefetch_q.append((tol,timestep+dt, x+dx, y+dy, z+dz))
+                            self.gonnaPrefetchSet.add((tol,timestep+dt, x+dx, y+dy, z+dz))
+
+    def pop_front(self):
+        return self.prefetch_q.popleft()
+    
+    def prefetch_q_empty(self):
+        if(len(self.prefetch_q) == 0):
+            return True
+        else:
+            return False
+
+    def fetch(self,blockId):
+        self.prefetchedSet.add(blockId)
+        self.Netif.send_req(blockId)
+
+    def InformL2MissByL1Pref(self,blockId):
+        print("L1 prefetcher missed to catch on L2 cache:{}\n".format(blockId))
+        # TODO 何かしらのプリフェッチポリシーの変更を加える必要
+        pass
+
+    def InformL2MissByUser(self,blockId):
+        print("L1 and L2 Missed the request by user:{}\n",blockId)
+        self.clearQueue()
+        self.enque_neighbor_blocks(blockId)
+
+    def InformL1MissL2HitByUser(self,blockId):
+        print("the data was catched in L2: {}\n".format(blockId))
+        # L2でぎりぎりキャッチできたので、何かしらのL2のプリフェッチポリシーをここで変更する必要があるかもしれない。
+        # とりあえず、プリフェッチQの中身を空にして、新たに、その地点からプリフェッチを開始する感じにするか。
+        self.clearQueue()
+        self.enque_neighbor_blocks(blockId)
+
+
+    async def fetchLoop(self):
+        while not self.stop_thread:
+            # print("L2:")
+            # self.L2Cache.printAllKeys()
+            if (not self.prefetch_q_empty()) and (self.L2Cache.usedSize < self.L2Cache.capacity):
+                # self.L2Cache.printInfo()
+                nextBlockId = self.pop_front()
+                self.Netif.send_req(nextBlockId) # 別スレッドで実行されるキュー
+                self.enque_neighbor_blocks(nextBlockId)
+            else:
+                await asyncio.sleep(0.1)  # Sleep for 1 second, or adjust as needed
+
+    def thread_func(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.fetchLoop())          
+
+    def startPrefetching(self):
+        self.stop_thread = False
+        if self.L2Cache.capacity > 0:
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            self.enqueue_first_blockId()
+            print("restarted L2 prefetcher!")
+        else :
+            print("L2 cache size is 0. So L2 prefetcher is not starting")
+
+    def stop(self):
+        self.stop_thread = True
+
+    def InitializeSetting(self,blockOffset):
+        self.prefetchedSet = set()
+        self.gonnaPrefetchSet = set()
+        self.prefetch_q = deque()
+        self.blockOffset = blockOffset
+
+    def changeBlockOffset(self,blockOffset):
+        self.blockOffset = blockOffset
+
+    def enqueue_first_blockId(self):
+        firstBlock = (0.1, 0, 0 ,0 ,0 )
+        self.prefetch_q.append(firstBlock)
+        self.gonnaPrefetchSet.add(firstBlock)
+
+    def clearQueue(self):
+        while not self.prefetch_q_empty():
+            blockId = self.pop_front()
+            self.gonnaPrefetchSet.discard(blockId)
+            self.Netif.sendQ.clear()
+
+# TODO 継承
+class L1Prefetcher:
+    def __init__(self,L1Cache,L2Cache,maxTimestep=9,offsetSize=256):
+        # 自分で持っていた方がいいと判断。
+        
+        self.L1Cache = L1Cache
+        self.L2Cache = L2Cache
+        self.decompressor = Decompressor(self.L1Cache)
+        # 自分で持つことにしました。
+        self.Netif = NetIF(self.L2Cache)
+
+        self.Tols = [0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5]
+        self.maxTimestep = maxTimestep
+        self.maxX = 1024
+        self.maxY = 1024
+        self.maxZ = 1024
+        self.default_offset = offsetSize
+        self.gonnaPrefetchSet = set()
+        self.prefetchedSet = set()
+        self.prefetch_q = deque()
+        self.stop_thread = False
+
+        # フェッチループ起動
+        if self.L1Cache.capacity == 0:
+            pass
+        else:
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            self.enqueue_first_blockId()
+
+    def enque_neighbor_blocks(self,centerBlock):
+
+        tol = centerBlock[0] 
+        timestep = centerBlock[1]
+        x = centerBlock[2]
+        y = centerBlock[3]
+        z = centerBlock[4]
+
+        ## TODO tolの扱い
+        for dt in [-1,0,1]:
+            for dx in [-self.default_offset, 0, self.default_offset]:
+                for dy in [-self.default_offset, 0, self.default_offset]:
+                    for dz in [-self.default_offset, 0, self.default_offset]:
+                        if (self.gonnaPrefetchSet.__contains__((tol,timestep+dt, x+dx, y+dy, z+dz))
+                            or (timestep+dt < 0) or (timestep+dt >= self.maxTimestep) 
+                            or (x+dx < 0) or (x+dx >= self.maxX)
+                            or (y+dy < 0) or (y+dy >= self.maxY)
+                            or (z+dz < 0) or (z+dz >= self.maxZ)):
+                            continue
+                        else:
+                            # print("appending {}".format((tol,timestep+dt, x+dx, y+dy, z+dz)))
+                            self.prefetch_q.append((tol,timestep+dt, x+dx, y+dy, z+dz))
+                            self.gonnaPrefetchSet.add((tol,timestep+dt, x+dx, y+dy, z+dz))
+
+
+    def enqueue_first_blockId(self):
+        firstBlock = (0.1, 0, 0 ,0 ,0 )
+        self.prefetch_q.append(firstBlock)
+        self.gonnaPrefetchSet.add(firstBlock)
+
+
+    def pop_front(self):
+        return self.prefetch_q.popleft()
+    
+    def letKnowCenterPoint(self,blockId):
+        pass
+
+    def InformL1MissByUser(self,blockId):
+        print("User missed to catch in L1: {}\n".format(blockId))
+        self.clearQueue()
+        self.enque_neighbor_blocks(blockId)
+
+    def InformL1MissAndL2Hit(self,blockId):
+        print("User missed to catch in L1 and Hit on L2: {}\n".format(blockId))
+
+    def InformL1MissAndL2Miss(self,blockId):
+        print("L1 miss and L2 miss: {}\n".format(blockId))
+        self.clearQueue()
+        self.enque_neighbor_blocks(blockId)
+    
+    def prefetch_q_empty(self):
+        if(len(self.prefetch_q) == 0):
+            return True
+        else:
+            return False
+    
+    async def fetchLoop(self):
+        while not self.stop_thread:
+            # print("L1 cache:")
+            # self.L1Cache.printAllKeys()
+            if (not self.prefetch_q_empty()) and (self.L1Cache.usedSize < self.L1Cache.capacity):
+                # self.L1Cache.printInfo()
+                nextBlockId = self.pop_front()
+                compressed = self.L2Cache.get(nextBlockId)
+                if compressed is None:
+                    # L2のプリフェッチポリシーを変更.どうやって？今までキューにたまっているものを捨てる？どうする？わかりまてー－ん。
+                    compressed = self.Netif.send_req_urgent(nextBlockId)
+                    original = self.decompressor.decompress(compressed)
+                    self.L1Cache.put(nextBlockId,original)
+                else: # L2Hit
+                    original = self.decompressor.decompress(compressed)
+                    self.L1Cache.put(nextBlockId,original)   
+
+                # 忘れずに！
+                self.enque_neighbor_blocks(nextBlockId)
+            else:
+                await asyncio.sleep(0.1)  # Sleep for 1 second, or adjust as needed
+
+    def thread_func(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.fetchLoop())       
+
+    def startPrefetching(self):
+        self.stop_thread = False
+        if self.L3Cache.capacity > 0:
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            self.enqueue_first_blockId()
+            print("restarted L3 prefetcher!")
+        else :
+            print("L3 cache size is 0. So L3 prefetcher is not starting")
+
+    def stop(self):
+        self.stop_thread = True
+
+    def InitializeSetting(self,blockOffset):
+        self.prefetchedSet = set()
+        self.gonnaPrefetchSet = set()
+        self.prefetch_q = deque()
+        self.blockOffset = blockOffset
+
+    def clearQueue(self):
+        while not self.prefetch_q_empty():
+            blockId = self.pop_front()
+            self.gonnaPrefetchSet.discard(blockId)
+
+
+# unit test
+if __name__ == "__main__":
+    print("test")
+
+
+
+         
+
+
+
---
+++ src/python/server/L3_L4prefetcher.py
@@ -0,0 +1,450 @@
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+import queue
+from collections import deque
+import asyncio
+import numpy as np
+import threading
+import time
+
+# self-defined libraries
+import _mgard as mgard
+from L3_L4Cache import LRU_cache
+from L3_L4Cache import dynamic_cache
+from slice import Slicer
+from compressor import compressor
+
+# L3キャッシュとL4キャッシュプリふぇっちゃで同じスライサーを共有すると、なぜか片方が全く取れなくなるという現象に遭遇
+class L3Prefetcher:
+    def __init__(self,L3Cache,L4Cache,L4Prefetcher,dataDim, blockOffset=256) -> None:
+        # ToleranceArray
+        self.Tols = [0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5]
+        self.dataDim = dataDim
+        self.maxTimestep = dataDim[0]
+        self.maxX = dataDim[1]
+        self.maxY = dataDim[2]
+        self.maxZ = dataDim[3]
+        self.blockOffset = blockOffset
+        self.L3Cache = L3Cache
+        self.L4Cache = L4Cache
+        # これね、L3PrefetcherとL4Prefetcherで別々のもの持ってないとなんか片方が一生使えなくなる
+        self.Slicer = Slicer(blockOffset=blockOffset)
+        self.compressor = compressor(self.L3Cache)
+        self.L4Pref = L4Prefetcher
+        self.gonnaPrefetchSet = set()
+        self.prefetchedSet = set()
+        self.prefetch_q = deque() # blocks going to get
+        self.thread = None
+        # キャッシュに入れるブロックの半径
+        self.radius = 3 # for now.
+
+        # スレッドを止めるためのフラグ
+        self.stop_thread = False
+
+        # フェッチループを起動
+        if self.L3Cache.capacity == 0:
+            # L3キャッシュが0の時は、L3Prefetcherはスタートしないと
+            pass
+        else :
+            print("start L3 prefetcher")
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            # 最初のブロックを投下
+            self.enqueue_first_blockId()
+        
+    # 距離パラメータを追加すればいいと思います！
+    def enque_neighbor_blocks(self,centerBlock,d):
+        tol = centerBlock[0] 
+        timestep = centerBlock[1]
+        x = centerBlock[2]
+        y = centerBlock[3]
+        z = centerBlock[4]
+        distance = d + 1
+
+        # 時間方向に1つづれたブロックは1ホップ
+        for dt in [-1,1]:
+            if (self.gonnaPrefetchSet.__contains__((tol,timestep+dt, x, y, z))
+                        or (timestep+dt < 0) or (timestep+dt >= self.maxTimestep)):
+                continue
+            else:
+                self.prefetch_q.append(((tol,timestep+dt, x, y, z),distance))
+                self.gonnaPrefetchSet.add((tol,timestep+dt, x, y, z))  
+
+        for dx in [-self.blockOffset, 0, self.blockOffset]:
+            for dy in [-self.blockOffset, 0, self.blockOffset]:
+                for dz in [-self.blockOffset, 0, self.blockOffset]:
+                    if (self.gonnaPrefetchSet.__contains__((tol,timestep, x+dx, y+dy, z+dz))
+                        or (x+dx < 0) or (x+dx >= self.maxX)
+                        or (y+dy < 0) or (y+dy >= self.maxY)
+                        or (z+dz < 0) or (z+dz >= self.maxZ)):
+                        continue
+                    else:
+                        self.prefetch_q.append(((tol,timestep, x+dx, y+dy, z+dz),distance))
+                        self.gonnaPrefetchSet.add((tol,timestep, x+dx, y+dy, z+dz))
+
+    def enqueue_first_blockId(self,blockId=(0.1, 0, 0 ,0 ,0 ),d=0):
+        self.prefetch_q.append((blockId,d))
+        self.gonnaPrefetchSet.add(blockId)
+
+    def pop_front(self):
+        return self.prefetch_q.popleft()
+    
+    def prefetch_q_empty(self):
+        if(len(self.prefetch_q) == 0):
+            return True
+        else:
+            return False
+        
+    def print_prefetch_q(self):
+        print(self.prefetch_q)
+
+
+
+    def getRadiusFromCapacity(self):
+        capacity = self.L3Cache.capacity
+        blockSize = self.blockOffset
+        # how to get radius? 4/3*pi*r**3が球の体積だけど、ここ、
+        # どうにかしてキャッシュの容量から、その容量を満たす半径を出したいんだよね。
+        # 2Dでは、8ブロックが隣接,3Dでは、26このブロックが隣接。4Dでは、80個らしい。
+        # n次元立方体の頂点の数は2^nで一般化できる。で、一つの頂点二着重くしたときに、これができれば、話は簡単だ
+        # おそらく。まて、
+        # n次元立方体の面の数は2nこ。
+        # 辺の数は、n*2^(n-1)こ
+        # 頂点の数は、2^n
+        # で、一つの立方体を考えると、面で接しているのが2n、辺で接しているのがn*2^(n-1)頂点で接しているのが、2^n
+        # つまり、2n + n*2^(n-1) + 2^n 
+        # n = 2の時、4 + 4 + 4 = 12いや間違ってるわ！
+        # 超体積から考えると、a^nなので、aが3の時、3^3 - 1 = 26
+        # 3^4 - 1 = 80
+        # 3^n - 1で合っているんじゃないか？そんな気がしてきたわ。26*3 + 2 も行けるね、4次元なら、
+        # これは、今自分がいる時間に26個隣接していて、隣接する時間それぞれに27 (26 + 1)あるからっていう式
+        # よくて、5ホップとかな気がする。
+        # ちなみに、4次元球の面積は、pi*pi*r*r*r*r/2
+        # また、4次元立方体の体積はa^4
+        # blockSize が合って、これは置いておいて、
+        # 例えば、16GiBって、何個の要素が入るのか？
+        # elements(個)*4(byte/個) = 16*1024*1024*1024 (bytes)
+        # elements = 4*1024*1024*1024個
+        # 4*1024*1024*1024 = a^4をとくとどうなる？
+        # 2^(8*4) = a^4
+        # a = 2^8 = 256だってさ。まじで？
+        # 今やりたいことは、キャッシュのサイズがあって、その周りなんホップまでそのキャッシュに入れられるか？のホップ数を
+        # 出したいんですよね。4次元で。とりあえず、一遍の長さ
+        # ホップするごとに増える要素数を数えればいいんだ
+        # n = 2 の時、　0ホップ　ー＞　1、 1ホップ　→　３、2ホップ、５
+        # n = 3の時0、０ホップ：１、1ホップ：２７、２ホップ、125(5*3)とかな気がしています。
+        # n = 4の時：0ホップ：1、1ホップ：81 (3*4):、2ホップ(5**4) = 625、3ホップ(7**4) = 2401、4ホップ(9**4) = 6551
+        # ホップ数は半径ですね。なるほど、こういう感じで増えていくのか。
+        # つまり、4次元空間で、半径2(今いるブロックから2ホップいるブロックから2ホップ以内の)のブロックを全部持ってこようと
+        # すると、なんと625個も必要になるんですね。これは大変ですね。
+        # 256**3*4*625 = 2**24*625 = 16*625MiB= 10GiBって感じですね。
+        # これ、半径を3にすると、どうなるんだろう？ってことで、1ブロックを256で計算した場合、
+        # 256**3*4*2401 = 16*2401MiB = 38416MiB = 38.4GiBって感じですね。これはいける。
+        # 半径を4にすると、どうなるのか？104.816GiBって感じになりました。だいぶ多いですね。Muffin2じゃないと評価が厳しいかもです。
+        # で、L1とL4は生データを保持しているので、サイズからすぐにホップ数が計算できると。
+        # 問題は、L3とL2よね。圧縮されている、さらに圧縮率にもばらつきがあるので、サイズから一位にホップ数を決めることができないのよね。
+        # まあそこは仕方がない。
+        radius = 10
+        return radius
+    
+    def stopPrefetchingAndChangeCenter(self,centerBlockId):
+        # まず、一回プリフェッチをやめる。
+        # prefetch_Qを空にする。これをやるだけで、いい？
+        # 新しいセンターポイントがわかる。
+        # 新しいセンターポイントとキャッシュに入っている各要素との距離(ホップ数を計算する)。ここのけいさんが難しい。
+        # どうやってやるのか俺にはわからん calcHops()で計算
+        # で、そのホップ数が、指定半径(ホップ数)よりも大きい場合にはキャッシュからリムーブする。
+        for blockId,blockValue in self.L3Cache.cache.items():
+            hops = self.calcHops(centerBlockId,blockId)
+            if hops > self.radius:
+                self.L3Cache.cache.pop(blockId)
+                self.prefetchedSet.discard(blockId)
+        
+        # enqueBlockId.enqueueが完了すれば、あとは自動でプリフェッチが始まる。
+        # てか、ミスした後にこれをプリフェッチキューに入れてももう遅いとは思うんだけどね。（笑）
+        self.enqueue_first_blockId(centerBlockId,0)
+
+
+    # これの実装が急務です。
+    # ここは、ホップの計算をどうやっているかに深く依存します。
+    # 時間を変化させたら、それはもう1ホップとしてカウントしましょう。
+    def calHops(self,centerBlockId,targetBlockId):
+        # 時間方向に何個ずれているかを確認まずは時間方向のホップ数があります。
+        timeHops = abs(centerBlockId[1]-targetBlockId[1])
+
+        # 空間方向のホップ数を確認します。どうやって？だいきくんに教わりましたが、
+        # 結局最大のホップ数は、ベクトルの最大成分になります。これ大事！
+        # なぜかというと、考えてください。
+        xHops = abs(centerBlockId[2]- targetBlockId[2])
+        yHops = abs(centerBlockId[3]- targetBlockId[3])
+        zHops = abs(centerBlockId[4]- targetBlockId[4])
+        spaceHops = max(xHops,yHops,zHops)//self.blockOffset # これでホップ数が出る
+        return timeHops + spaceHops
+
+    # ここでどういう風にプリフェッチポリシーを変えるかというのもなかなか見ものであるところです
+    def InformL3Hit(self,blockId):
+        print("L3 hit ! nice! From L3 prefetcher")
+        pass
+
+    def InformL3MissAndL4Hit(self,blockId):
+        print("L3Miss and L4 Hit")
+    
+    def InformL3MissAndL4Miss(self,blockId):
+        print("L3 Missed and L4 Miss:{}\n".format(blockId))
+        # TODO 何かしらのプリフェッチポリシーの変更を加える必要
+        pass
+
+    def InformL4MissByPref(self,blockId):
+        print("L3 Prefetcher missed L4 and brought from disk:{}\n",blockId)
+
+    
+    async def fetchLoop(self):
+        while not self.stop_thread:
+            # print("L3 cache:")
+            # self.L3Cache.printInfo()
+            # self.L3Cache.printAllKeys()
+            if (not self.prefetch_q_empty()) and (len(self.L3Cache.cache) < self.L3Cache.capacity):
+                nextBlockId,distance = self.pop_front()
+                if distance > self.radius:
+                    continue
+                original = self.L4Cache.get(nextBlockId)
+                if original is None:
+                    try:
+                        self.L4Pref.prefetch_q.remove(nextBlockId)
+                        print("succeed in deleting {} in L4's prefetch Q".format(nextBlockId))
+                    except Exception as e:
+                        pass
+                    d = self.Slicer.sliceData(nextBlockId)
+                    # print("nextBlockId=",nextBlockId)
+                    # print("blocks size=",d.nbytes)
+                    # write to L4 also
+                    self.L4Cache.put(nextBlockId,d)
+                    tol = nextBlockId[0]
+                    compressed = self.compressor.compress(d,tol)
+                    self.L3Cache.put(nextBlockId,compressed)
+                else:
+                    print("L4 HIT! when L3 Prefetching from L4",nextBlockId,"distance:",distance)
+                    tol = nextBlockId[0]
+                    compressed = self.compressor.compress(original,tol)
+                    self.L3Cache.put(nextBlockId,compressed)
+                self.prefetchedSet.add(nextBlockId)
+                self.enque_neighbor_blocks(nextBlockId,distance)
+            else:
+                await asyncio.sleep(0.1)  # Sleep for 1 second, or adjust as needed
+    
+    def startPrefetching(self):
+        self.stop_thread = False
+        if self.L3Cache.capacity > 0:
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            self.enqueue_first_blockId()
+            print("restarted L3 prefetcher!")
+        else :
+            print("L3 cache size is 0. So L3 prefetcher is not starting")
+
+    def stop(self):
+        self.stop_thread = True
+
+    def InitializeSetting(self,blockOffset):
+        self.Slicer.changeBlockSize(blockOffset)
+        self.prefetchedSet = set()
+        self.gonnaPrefetchSet = set()
+        self.prefetch_q = deque()
+        self.blockOffset = blockOffset
+
+    def changeBlockOffset(self,blockOffset):
+        self.blockOffset = blockOffset
+
+    def thread_func(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.fetchLoop())          
+
+
+# TODO 継承
+class L4Prefetcher:
+    def __init__(self,L4Cache,dataDim,blockSize):
+        self.L4Cache = L4Cache
+        self.Slicer = Slicer(blockOffset=blockSize)
+        self.Tols = [0.0001,0.001,0.01,0.1,0.2,0.3,0.4,0.5]
+        self.dataDim = dataDim
+        self.maxTimestep = dataDim[0]
+        self.maxX = dataDim[1]
+        self.maxY = dataDim[2]
+        self.maxZ = dataDim[3]
+        self.blockOffset = blockSize
+        self.gonnaPrefetchSet = set() # プリフェッチしに行くセット
+        self.prefetchedSet = set() # プリフェッチしたセット
+        self.prefetch_q = deque()
+        self.stop_thread = False
+        self.thread = None
+
+        self.radius = 4
+
+        # フェッチループ起動
+        if self.L4Cache.capacity == 0:
+            pass
+        else :
+            print("start L4 prefetcher")
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            self.enqueue_first_blockId()
+
+
+
+# ここのforループの順番を変えることで、時間なのか、空間なのか、どっちの情報を優先的に取ってくるのかを決められる。どうするか？
+    def enque_neighbor_blocks(self,centerBlock,d):
+        tol = centerBlock[0] 
+        timestep = centerBlock[1]
+        x = centerBlock[2]
+        y = centerBlock[3]
+        z = centerBlock[4]
+        distance = d + 1
+
+        # 時間方向に1つづれたブロックは1ホップ
+        for dt in [-1,1]:
+            if (self.gonnaPrefetchSet.__contains__((tol,timestep+dt, x, y, z))
+                        or (timestep+dt < 0) or (timestep+dt >= self.maxTimestep)):
+                continue
+            else:
+                self.prefetch_q.append(((tol,timestep+dt, x, y, z),distance))
+                self.gonnaPrefetchSet.add((tol,timestep+dt, x, y, z))  
+
+        for dx in [-self.blockOffset, 0, self.blockOffset]:
+            for dy in [-self.blockOffset, 0, self.blockOffset]:
+                for dz in [-self.blockOffset, 0, self.blockOffset]:
+                    if (self.gonnaPrefetchSet.__contains__((tol,timestep, x+dx, y+dy, z+dz))
+                        or (x+dx < 0) or (x+dx >= self.maxX)
+                        or (y+dy < 0) or (y+dy >= self.maxY)
+                        or (z+dz < 0) or (z+dz >= self.maxZ)):
+                        continue
+                    else:
+                        self.prefetch_q.append(((tol,timestep, x+dx, y+dy, z+dz),distance))
+                        self.gonnaPrefetchSet.add((tol,timestep, x+dx, y+dy, z+dz))
+
+    def pop_front(self):
+        return self.prefetch_q.popleft()
+    
+    def prefetch_q_empty(self):
+        if(len(self.prefetch_q) == 0):
+            return True
+        else:
+            return False
+        
+    def print_prefetch_q(self):
+        print(self.prefetch_q)
+
+    def calHops(self,centerBlockId,targetBlockId):
+        timeHops = abs(centerBlockId[1]-targetBlockId[1])
+        xHops = abs(centerBlockId[2]- targetBlockId[2])
+        yHops = abs(centerBlockId[3]- targetBlockId[3])
+        zHops = abs(centerBlockId[4]- targetBlockId[4])
+        spaceHops = max(xHops,yHops,zHops)//self.blockOffset # これでホップ数が出る
+        return timeHops + spaceHops
+
+    def enqueue_blockId(self,blockId):
+        self.prefetch_q.append(blockId)
+
+    def enqueue_first_blockId(self,blockId = (0.1, 0, 0 ,0 ,0 ),d=0):
+        self.prefetch_q.append((blockId,d))
+        self.gonnaPrefetchSet.add(blockId)
+
+    def InformL3Hit(self,blockId):
+        print("L3 hit! nice! from L4 prefetcher")
+        pass
+
+    def InformL3MissAndL4Hit(self,blockId):
+        print("L3Miss and L4 Hit")
+    
+    def InformL3MissAndL4Miss(self,blockId):
+        print("L3 Miss and L4 Miss:{}\n".format(blockId))
+        # TODO 何かしらのプリフェッチポリシーの変更を加える必要
+        pass
+
+    def InformL4MissByPref(self,blockId):
+        print("L3 Prefetcher missed L4 and brought from disk:{}\n",blockId)
+
+    def clearPrifetchingQ(self):
+        self.prefetch_q = deque()
+
+    async def fetchLoop(self):
+        while not self.stop_thread:
+            # print("L4 cache:")
+            # self.L4Cache.printInfo()
+            # self.L4Cache.printAllKeys()
+            if (not self.prefetch_q_empty()) and (len(self.L4Cache.cache) < self.L4Cache.capacity):
+                nextBlockId,distance = self.pop_front()
+                if distance > self.radius:
+                    continue
+                data = self.Slicer.sliceData(nextBlockId)
+                self.L4Cache.put(nextBlockId,data)
+                self.prefetchedSet.add(nextBlockId)
+                self.enque_neighbor_blocks(nextBlockId,distance)
+            else:
+                await asyncio.sleep(0.1)  # Sleep for 1 second, or adjust as needed
+    
+
+    def startPrefetching(self):
+        self.stop_thread = False
+        if self.L4Cache.capacity > 0:
+            self.thread = threading.Thread(target=self.thread_func)
+            self.thread.start()
+            self.enqueue_first_blockId()
+            print("restarted L4 prefetcher!")
+        else :
+            print("L4 cache size is 0. So L4 prefetcher is not starting")
+        
+    def changeBlockOffset(self,blockOffset):
+        self.blockOffset = blockOffset
+
+    def stop(self):
+        self.stop_thread = True
+
+    def InitializeSetting(self,blockOffset):
+        self.Slicer.changeBlockSize(blockOffset)
+        self.prefetchedSet = set()
+        self.gonnaPrefetchSet = set()
+        self.prefetch_q = deque()
+        self.blockOffset = blockOffset
+
+    def thread_func(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.fetchLoop())       
+
+  
+
+def calHops(centerBlockId,targetBlockId):
+        # 時間方向に何個ずれているかを確認まずは時間方向のホップ数があります。
+        timeHops = abs(centerBlockId[1]-targetBlockId[1])
+
+        # 空間方向のホップ数を確認します。どうやって？だいきくんに教わりましたが、
+        # 結局最大のホップ数は、ベクトルの最大成分になります。これ大事！
+        # なぜかというと、考えてください。
+        xHops = abs(centerBlockId[2]- targetBlockId[2])
+        yHops = abs(centerBlockId[3]- targetBlockId[3])
+        zHops = abs(centerBlockId[4]- targetBlockId[4])
+        spaceHops = max(xHops,yHops,zHops)//256 # これでホップ数が出る
+        return timeHops + spaceHops
+
+
+
+if __name__ == "__main__":
+    start = time.time()
+    test1 = (0.1, 0, 256, 256, 256)
+    test2 = (0.1, 0, 768, 256, 512)
+    test3 = (0.1, 2, 256, 256, 256)
+    center = (0.1,0,0,0,0)
+    print(calHops(center,test1))
+    print(calHops(center,test2))
+    print(calHops(test1,test2))
+    print(calHops(center,test3))
+    print(calHops(test3,center))
+
+    end = time.time()
+    print(end - start)
+         
+
+
+
