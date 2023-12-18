b8103898e2a3eafdbd621c5da07405c9634c42a9
martin-ray <aorayagi@gmail.com>
2023-12-18 20:53:32 +0900

    add tiledb slice test. TileDB is amazingly scalable.

--- src/python/server/slice.py
+++ src/python/server/slice.py
@@ -3,6 +3,9 @@ import numpy as np
 import _mgard as mgard
 import tiledb
 import threading
+import time
+import threading
+
 
 class Slicer:
     # default 引数
@@ -60,22 +63,100 @@ class Slicer:
 
 
 class TileDBSlicer:
-    def __init__(self,blockOffset=256,filename="/scratch/aoyagir/step1_256_test_0902.h5") -> None:
+    def __init__(self,blockOffset=256,cacheSize=0,filename="/scratch/aoyagir/minio_data/tiledb/array5") -> None:
+        # Create a TileDB config
+        self.config = tiledb.Config()
         self.filename = filename
         self.blockOffset = blockOffset
-        self.file = h5py.File(filename, 'r')
-        self.dataset = self.file['data']
-        self.timesteps = self.dataset.shape[0]
-        self.xMax = self.dataset.shape[1]
-        self.yMax = self.dataset.shape[2]
-        self.zMax = self.dataset.shape[3]
+        self.timesteps = None
+        self.xMax = None
+        self.yMax = None
+        self.zMax = None
         self.lock = threading.Lock()
         self.printDataInfo()
+        self.ctx = tiledb.Ctx(self.config)
 
     def printDataInfo(self):
         print("##### Row data INFO #####")
         print("timesteps:{}\nx:{}\ny:{}\nz:{}\n"
               .format(self.timesteps,self.xMax,self.yMax,self.zMax))
-    
-    pass
+
+    def getDim(self):
+        with tiledb.open(self.filename, mode="r", ctx=self.ctx) as array:
+            # Retrieve and print the array schema
+            array_schema = array.schema
+            print("Array Schema:")
+            print(array_schema)
+            # You can also access other information about the array, such as its domain and attributes
+            domain = array_schema.domain
+            print("Domain:", domain)
+
+    def get(self,timestep,x,xx,y,yy,z,zz):
+        with tiledb.DenseArray(self.filename, mode="r", ctx=self.ctx) as array:
+            # Read the sliced data from the TileDB array using 'from' and 'to' expressions
+            data = array[timestep,x:xx, y:yy, z:zz]
+            return data
+
+
+
+import concurrent.futures
+class TileDBReaderThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.tiledb_reader = TileDBSlicer()
+
+    def run(self):
+        a = self.tiledb_reader.get(0, 0, 256, 0, 256, 0, 256)
+        for _ in range(100):
+            s = time.time()
+            a = self.tiledb_reader.get(0, 0, 256, 0, 256, 0, 256)
+            e = time.time()
+            print(e - s)
+        print(a)
+
+
+# test
+class TileDBReader:
+    def __init__(self):
+        self.tiledb_reader = TileDBSlicer()
+
+    def read_and_measure(self, thread_id):
+        a = self.tiledb_reader.get(0, 0, 256, 0, 256, 0, 256)
+        i = thread_id
+        for _ in range(100):
+            s = time.time()
+            if thread_id <=3:
+                a = self.tiledb_reader.get(0, i*256, i*256 + 256, i*256, i*256 + 256, i*256, i*256 + 256)
+            else :
+                i -=3
+                a = self.tiledb_reader.get(0, i*256, i*256 + 256, i*256, i*256 + 256, i*256, i*256 + 256)
+            e = time.time()
+            print(f"Thread {thread_id}: {e - s} seconds")
+
+class HDF5Reader:
+    def __init__(self):
+        self.hdf5_reader = Slicer()
+
+    def read_and_measure(self, thread_id):
+        a = self.hdf5_reader.sliceData((0.1, 0, 0, 0, 0))
+        for _ in range(100):
+            s = time.time()
+            a = self.hdf5_reader.sliceData((0.1, 0,0,0,0))
+            e = time.time()
+            print(f"Thread {thread_id}: {e - s} seconds")
+
+
+if __name__ == "__main__":
+    num_threads = 8  # Adjust the number of threads as needed
+    tiledb_readers = [TileDBReader() for _ in range(num_threads)]
+    hdf5_readers = [HDF5Reader() for _ in range(num_threads)]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        for i in range(num_threads):
+            executor.submit(tiledb_readers[i].read_and_measure, i)
+
+    print("HDF5 scalability")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        for i in range(num_threads):
+            executor.submit(hdf5_readers[i].read_and_measure, i)
 
