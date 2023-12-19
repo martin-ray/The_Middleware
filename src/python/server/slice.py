2ff9a4b7813501b6bfd1dee52467bdd7101cf943
martin-ray <aorayagi@gmail.com>
2023-12-19 14:43:36 +0900

    add tiledb slicer.

--- src/python/server/slice.py
+++ src/python/server/slice.py
@@ -63,18 +63,19 @@ class Slicer:
 
 
 class TileDBSlicer:
-    def __init__(self,blockOffset=256,cacheSize=0,filename="/scratch/aoyagir/minio_data/tiledb/array5") -> None:
+    def __init__(self,blockOffset=256,cacheSize=0,filename="/scratch/aoyagir/tiledb_data/array64") -> None:
         # Create a TileDB config
         self.config = tiledb.Config()
+        self.ctx = tiledb.Ctx(self.config)
         self.filename = filename
         self.blockOffset = blockOffset
         self.timesteps = None
         self.xMax = None
         self.yMax = None
         self.zMax = None
-        self.lock = threading.Lock()
+        self.getDim()
         self.printDataInfo()
-        self.ctx = tiledb.Ctx(self.config)
+
 
     def printDataInfo(self):
         print("##### Row data INFO #####")
@@ -91,72 +92,21 @@ class TileDBSlicer:
             domain = array_schema.domain
             print("Domain:", domain)
 
-    def get(self,timestep,x,xx,y,yy,z,zz):
+    def slice_single_step(self,timestep,x,xx,y,yy,z,zz):
         with tiledb.DenseArray(self.filename, mode="r", ctx=self.ctx) as array:
             # Read the sliced data from the TileDB array using 'from' and 'to' expressions
-            data = array[timestep,x:xx, y:yy, z:zz]
+            # arrayから返却されるのは、orderd_dict.で、keyが'data'になっているので読みだす必要がある。
+            # 以下で、dataが読み出される。
+            data = array[timestep,x:xx, y:yy, z:zz]['data']
             return data
-
-
-
-import concurrent.futures
-class TileDBReaderThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
-        self.tiledb_reader = TileDBSlicer()
-
-    def run(self):
-        a = self.tiledb_reader.get(0, 0, 256, 0, 256, 0, 256)
-        for _ in range(100):
-            s = time.time()
-            a = self.tiledb_reader.get(0, 0, 256, 0, 256, 0, 256)
-            e = time.time()
-            print(e - s)
-        print(a)
-
-
-# test
-class TileDBReader:
-    def __init__(self):
-        self.tiledb_reader = TileDBSlicer()
-
-    def read_and_measure(self, thread_id):
-        a = self.tiledb_reader.get(0, 0, 256, 0, 256, 0, 256)
-        i = thread_id
-        for _ in range(100):
-            s = time.time()
-            if thread_id <=3:
-                a = self.tiledb_reader.get(0, i*256, i*256 + 256, i*256, i*256 + 256, i*256, i*256 + 256)
-            else :
-                i -=3
-                a = self.tiledb_reader.get(0, i*256, i*256 + 256, i*256, i*256 + 256, i*256, i*256 + 256)
-            e = time.time()
-            print(f"Thread {thread_id}: {e - s} seconds")
-
-class HDF5Reader:
-    def __init__(self):
-        self.hdf5_reader = Slicer()
-
-    def read_and_measure(self, thread_id):
-        a = self.hdf5_reader.sliceData((0.1, 0, 0, 0, 0))
-        for _ in range(100):
-            s = time.time()
-            a = self.hdf5_reader.sliceData((0.1, 0,0,0,0))
-            e = time.time()
-            print(f"Thread {thread_id}: {e - s} seconds")
-
-
-if __name__ == "__main__":
-    num_threads = 8  # Adjust the number of threads as needed
-    tiledb_readers = [TileDBReader() for _ in range(num_threads)]
-    hdf5_readers = [HDF5Reader() for _ in range(num_threads)]
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        for i in range(num_threads):
-            executor.submit(tiledb_readers[i].read_and_measure, i)
-
-    print("HDF5 scalability")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        for i in range(num_threads):
-            executor.submit(hdf5_readers[i].read_and_measure, i)
+    
+    # blockId = (tol,timestep,x,y,z)
+    def sliceData(self,blockId):
+        # with self.lock:
+        _ = blockId[0]
+        t = blockId[1]
+        x = blockId[2]
+        y = blockId[3]
+        z = blockId[4]
+        return self.slice_single_step(t,x,x+self.blockOffset,y,y+self.blockOffset,z,z+self.blockOffset)
 
